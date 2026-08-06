1413effe8f37a11749bd126e09b821e813203de1
Internetezoo <123302216+Internetezoo@users.noreply.github.com>
2026-08-06 12:23:13 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('ip').innerText = data.ip;
-  })
-  .catch(error => {
-    document.getElementById('ip').innerText = 'Hiba a lekérés során';
-  });
